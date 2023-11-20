168438317e98df66d4a92d5b2001eb2b20f31e3d
Matheus <matheusmol@hotmail.com>
2023-11-20 00:36:06 -0300

    Add handler

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 type Some<T> = { _type: "some"; value: T };
-type Err = { _type: "err"; message: any };
+type Err = { _type: "err"; err: any };
 type Options<T> = Some<T> | Err;
 
 export class Result<T> {
@@ -9,7 +9,7 @@ export class Result<T> {
   constructor(func: (...args: any[]) => T, ...args: any[]) {
     this.func = func;
     this.args = args;
-    this.option = { _type: "err", message: "error" };
+    this.option = { _type: "err", err: "error" };
   }
 
   async run() {
@@ -18,7 +18,7 @@ export class Result<T> {
       const response = Some(await this.func(...this.args));
       this.option = response;
     } catch (e: any) {
-      const err: Err = { _type: "err", message: e.message };
+      const err: Err = { _type: "err", err: e };
       this.option = err;
     }
     return this;
@@ -32,13 +32,13 @@ export class Result<T> {
       isNaN(Number(this.option.value))
     ) {
       if (message) {
-        throw new Error(message);
+        throw new Handler(new Error(""), message);
       }
-      throw new Error("method return NaN");
+      throw new Handler(new Error(""), "method return NaN");
     } else if (this.option._type === "err") {
-      throw new Error(message || this.option.message);
+      throw new Handler(this.option.err, message);
     } else {
-      throw new Error("Error with expect method");
+      throw new Handler(new Error("Error with expect method"));
     }
   }
 
@@ -51,9 +51,33 @@ export class Result<T> {
   }
 }
 
+class Handler {
+  constructor(error: any, message?: string) {
+    const date_time = new Date().toDateString();
+    const function_name = error.stack?.split("at ")[1]?.split(" ")[0];
+    const line = error.stack?.split("at ")[1]?.split(":")[1];
+    const file = error.stack
+      ?.split("at ")[1]
+      ?.split(":")[0]
+      ?.split("(")[1]
+      ?.trim();
+    console.log(
+      `Error: date_time: ${date_time} fuction: ${function_name} line ${line} file: ${file}`,
+    );
+    console.log(`Message: ${message || error.message}`);
+    throw new Error(message || error.message);
+    // process.exit(1);
+  }
+}
+
 import test from "node:test";
 import * as assert from "node:assert";
 const sum = (a: number, b: number) => Number(a) + b;
+
+test("should return null using unwarp", async () => {
+  const result = (await new Result(sum, "e", 2).run()).unwrap();
+  assert.strictEqual(result, null);
+});
 
 test("should trow error with expect message", async () => {
   try {
@@ -64,19 +88,13 @@ test("should trow error with expect message", async () => {
   }
 });
 
-test("should return null using unwarp", async () => {
-  const result = (await new Result(sum, "e", 2).run()).unwrap();
-  assert.strictEqual(result, null);
-});
-
 test("Should work fine with unwarp", async () => {
   const result = (await new Result(sum, 2, 2).run()).unwrap();
   assert.strictEqual(result, 4);
 });
 
 test("Should work fine with expect", async () => {
-  const result = (await new Result(sum, 2, 2).run()).expect(
-    "Error with sum method",
-  );
+  //prettier-ignore
+  const result = (await new Result(sum, 2, 2).run()).expect("Error with sum method");
   assert.strictEqual(result, 4);
 });
